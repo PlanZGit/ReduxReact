a15388ce66feba1dfd21940fca2a450862ba97a1
Vang Xiong <vangx709@gmail.com>
2023-02-20 16:45:15 -0900

    Middleware, logger, devtools, action payload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import HookCakeContainer from "./components/HookCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <NewCakeContainer />
         <CakeContainer />
         <HookCakeContainer />
         <IceCreamContainer />
